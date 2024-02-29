38e282052c855e9e3ef2a9af66c3a1ca7aee899d
Thống Nguyễn <thong0128@gmail.com>
2024-02-29 15:13:13 +0700

    Done

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -1,10 +1,14 @@
-package com.codegym.DAO;
+package com.codegym.controller;
 
+import com.codegym.DAO.UserDAO;
 import com.codegym.model.Login;
 import com.codegym.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class UserController {
@@ -12,6 +16,18 @@ public class UserController {
     public ModelAndView home() {
         ModelAndView mav = new ModelAndView("home");
         mav.addObject("login", new Login());
+        return mav;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login) {
+        User user = UserDAO.checkLogin(login);
+        ModelAndView mav;
+        if (user == null) {
+            mav = new ModelAndView("error");
+        }else {
+            mav = new ModelAndView("user");
+            mav.addObject("user", user);
+        }
         return mav;
     }
 }
